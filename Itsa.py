27f47b125b7893e771e1028d1edac32539e07454
Bcevallosh <alternacevallos@gmail.com>
2024-04-28 11:27:44 -0500

    Mi primer ejercicio en Linux

---
+++ Itsa.py
@@ -0,0 +1,10 @@
+numero=int(input('Ingrese el numero: '))
+
+if numero>5:
+    print('El  numero es mayor a 5')
+
+elif numero ==5:
+    print('El numero es igual a 5')
+
+else:
+    print('El numero es menor a 5')
