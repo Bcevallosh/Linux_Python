527584a1fdf4f63fc34911d9956041e4ff5041bd
Bcevallosh <alternacevallos@gmail.com>
2024-05-27 10:49:19 -0500

    Calcular el area de un circulo con la libreria math

---
+++ Python desde cero/2_Area_de_circulo.py
@@ -0,0 +1,4 @@
+import math # Importo la liberia math
+radio = 3
+area  = math.pi * radio ** 2 # utilizamos math.pi para multiplicarlo por el radio al cuadrado
+print("El area del circulo es ", area)
